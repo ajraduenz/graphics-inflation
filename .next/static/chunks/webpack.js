11abce53ac9ecf69fe56fdf86685d334c37a4cd1
ajraduenz <aldoraduenzjunior@gmail.com>
2022-10-24 23:02:54 -0300

    edit gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "070d7299cb42d8a2"; }
+/******/ 		__webpack_require__.h = function() { return "b7fe577d82598995"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
